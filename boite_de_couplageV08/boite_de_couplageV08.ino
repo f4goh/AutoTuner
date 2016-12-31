a9900856a927e853665dda11e456374b34fdd5fe
F4GOH <f4goh@orange.fr>
2016-12-31 11:15:40 +0100

    update
    version update 31/12/2016

--- boite_de_couplageV08/boite_de_couplageV08.ino
+++ boite_de_couplageV08/boite_de_couplageV08.ino
@@ -2,7 +2,13 @@
   Boite de couplage automatique
   version boitier étanche avec com RS485
  
-  V0.4 : recherche a partir de l'ATU balise
+  V0.8 : recherche a partir de l'ATU balise
+  2x 8 relais attention actifs au NL1
+  2x 2 relais attention actifs au NL0 (en fonction du modèle)
+  debug rs485 en cours
+  
+  
+  
 
 Commande rs485 boitier ou USB (PC)
 
@@ -65,9 +71,16 @@ T 30   trig power level
 
 // include the library code:
 
+//#define AFFLCD
+
 #include <EEPROM.h>
 #include <Wire.h> 
-//#include <LiquidCrystal_I2C.h>
+
+
+#ifdef AFFLCD
+ #include <LiquidCrystal_I2C.h>
+#endif
+
 
 //#define DEBUG_CMD
 //#define DEBUG_SWR
@@ -75,7 +88,7 @@ T 30   trig power level
 
 #define measureInterval 100
 #define searchDelay 20
-
+//#define searchDelay 1
 
 // initialize the lcd 
 //LiquidCrystal_I2C lcd(0x27, 20, 4);
@@ -83,8 +96,8 @@ T 30   trig power level
 // init keypad
 int lcd_key     = 0;
 int adc_key_in  = 0;
-#define balun_pin 2
-#define hzlz_pin  3
+#define balun_pin 3
+#define hzlz_pin  2
 #define rede_pin  4
 #define tx_soft_pin  7
 #define rx_soft_pin  8
@@ -95,7 +108,8 @@ int adc_key_in  = 0;
 #define vf A0
 #define vr A1
 
-
+#define relaylEnable HIGH
+#define relaylDisable LOW
 
 typedef struct  {
   byte id;            //indentification match box default 0xfd
@@ -112,8 +126,39 @@ AtuStruct Atu;    //declare la structure 1+1+4+1+1+1+1+1+4=15 bytes
 
 #define AtuSize 15
 
-#define LRAdr 0x20    //adresse i2c mcp23008
-#define CRAdr 0x21
+#define McpPcf 1        //0 MCP, 1 PCF
+
+#define mcpLRAdr 0x20    //adresse i2c mcp23008
+#define mcpCRAdr 0x21
+
+// PCF8574  Address range is 0x20-0x27
+// PCF8574A Address range is 0x38-0x3F
+
+#define pcfLRAdr 0x38    //adresse i2c PCF8574AT
+#define pcfCRAdr 0x20    //adresse i2c PCF8574T
+
+#ifdef AFFLCD
+  LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+  byte car0[8] = {B00000,B00000,B00000,B00000,B00000,B00000,B00000,B00000};  //declare en global pour allez plus vite
+  byte car1[8] = {B10000,B10000,B10000,B10000,B10000,B10000,B10000,B10000};
+  byte car2[8] = {B11000,B11000,B11000,B11000,B11000,B11000,B11000,B11000};
+  byte car3[8] = {B11100,B11100,B11100,B11100,B11100,B11100,B11100,B11100};
+  byte car4[8] = {B11110,B11110,B11110,B11110,B11110,B11110,B11110,B11110};
+  byte car5[8] = {B11111,B11111,B11111,B11111,B11111,B11111,B11111,B11111};
+
+typedef struct  {
+  byte swrPrec;
+  byte swr;            //read swr from match box  
+  int pPrec;              //read power from box
+  int p;              //read power from box
+  }AtuLcdStruct;
+AtuLcdStruct AtuLcd;    //declare la structure
+
+
+#endif
+
+
 
 byte cval=0;
 byte lval=0;
@@ -131,13 +176,7 @@ byte Research=0;
 void setup() {
   // set up the LCD's number of columns and rows: 
   Wire.begin(); 
-  //  lcd.begin();
-  //  lcd.backlight();
   Serial.begin(9600);
-  // Print a message to the LCD.
-  //  lcd.print("BOITE AUTO");
-  //  lcd.print("F4GOH/F1BJD");
-  //  pinMode(wakeup, INPUT);     
   analogReference(EXTERNAL);
 
   pinMode(balun_pin, OUTPUT);     
@@ -146,23 +185,53 @@ void setup() {
   pinMode(led_red, OUTPUT);
   pinMode(led_green, OUTPUT);
 
-  digitalWrite(balun_pin, LOW);
-  digitalWrite(hzlz_pin, LOW);
+  digitalWrite(balun_pin, relaylDisable);
+  digitalWrite(hzlz_pin, relaylDisable);
   digitalWrite(rede_pin, LOW);
   digitalWrite(led_red, HIGH);
   digitalWrite(led_green, LOW);
 
+  #ifdef AFFLCD
+    lcd.begin();
+    lcd.setBacklight(HIGH);    
+    lcd.clear();
+    lcd.print(F(" AUTOMATIC ATU v1.0"));    //intro
+    lcd.setCursor(0, 2);
+    lcd.print(F("     F4GOH 2016"));
+    delay(4000);
+    lcd.clear();
+    lcd.createChar(0, car0);
+    lcd.createChar(1, car1);
+    lcd.createChar(2, car2);
+    lcd.createChar(3, car3);
+    lcd.createChar(4, car4);
+    lcd.createChar(5, car5);
+  #endif
+  
 
-  send_gpio(LRAdr, 0x00, 0x00);  //init gpio L to output
-  send_gpio(CRAdr, 0x00, 0x00);  //init gpio C to output
+if (McpPcf==0){
+  sendGpioMcp(mcpLRAdr, 0x00, 0x00);  //init gpio L to output
+  sendGpioMcp(mcpCRAdr, 0x00, 0x00);  //init gpio C to output
 
-  send_gpio(LRAdr, 0x09, 0x00);  //all gpio L at level 0
-  send_gpio(CRAdr, 0x09, 0x00);  //all gpio C at level 0
+  sendGpioMcp(mcpLRAdr, 0x09, 0xff);  //all gpio L at level 0
+  sendGpioMcp(mcpCRAdr, 0x09, 0xff);  //all gpio C at level 0
+}
+else
+{
+  sendGpioPCF(pcfLRAdr,0xff);
+  sendGpioPCF(pcfCRAdr,0xff);
+}
 
 
     check_eeprom();
   #ifdef DEBUG_CMD
     commandDebug();
+  #endif
+
+   #ifdef AFFLCD
+  printParameters();
+  delay(2000);
+  printSearchmenu();
   #endif
 }
 
@@ -170,18 +239,136 @@ void setup() {
 
 void loop() {
 
-  checkSwr();
+  checkSwr(0);
   commandInput();
+
 }
+
+//--------------------------------------- gestion de l'affichage
+#ifdef AFFLCD
+void printParameters()
+{
+lcd.setCursor(0,0); 
+lcd.print(F("Balun:    Save:"));
+lcd.setCursor(0,1); 
+lcd.print(F("Pont:  dbm"));
+lcd.setCursor(0,2); 
+lcd.print(F("Trig power:"));
+lcd.setCursor(0,3); 
+lcd.print(F("Swr max level:"));
+
+lcd.setCursor(6,0);
+if (Atu.balun==1) lcd.print(F("on")); else lcd.print(F("off"));
+lcd.setCursor(15,0);
+if (Atu.saveAuto==1) lcd.print(F("auto")); else lcd.print(F("none"));
+lcd.setCursor(5,1);
+lcd.print(Atu.OffsetdBmBridge);
+lcd.setCursor(11,2);
+lcd.print((int)Atu.Pset/10);
+lcd.print(F("."));
+lcd.print((int) Atu.Pset%10);
+lcd.print(F("W"));
+lcd.setCursor(14,3);
+lcd.print((int)Atu.swrSet/10);
+lcd.print(F("."));
+lcd.print((int)Atu.swrSet%10);
+delay(2000);
+printSearchmenu();
+}
+
+void printSearchmenu()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(F("SWR"));
+  lcd.setCursor(0, 2);
+  lcd.print(F("Power:"));
+}
+
+//--------------------------------------- gestion de la commande d'affichage du swr
+
+void PrintSwr()
+{
+
+if (AtuLcd.swrPrec!=AtuLcd.swr){
+lcd.setCursor(5,0);
+lcd.print((int) AtuLcd.swr/10);
+lcd.print(F("."));
+lcd.print((int) AtuLcd.swr%10);
+//lcd.print("  ");
+//Atu.swrPrec=Atu.swr;
+Bargraph(AtuLcd.swr,1,1);
+}
+}
+
+
+void PrintPwr()
+{
+if (AtuLcd.pPrec!=AtuLcd.p){
+lcd.setCursor(7,2);
+lcd.print(AtuLcd.p/10);
+lcd.print(F("."));
+lcd.print(AtuLcd.p%10);
+lcd.print("W  ");
+AtuLcd.pPrec=AtuLcd.p;
+}
+
+}
+
+
+
+void Bargraph(int swr, int ligne_curseur, byte large_or_fine)
+{
+int curseur_map;
+
+ // curseur swr depart 6 fin 19 (1ong car 19-6+1=14) 5*14=70 avec -1 marge pour pas déborder
+if (large_or_fine==1){
+        if (swr>=100) swr=100;                //changement d'échelle pour recherche large (swr max 10)
+        curseur_map=map(swr,10,100,1,100-1);    
+      }
+      else
+      {
+        if (swr>=40) swr=40;                  //changement d'échelle pour recherche fine (swr max 4)
+        curseur_map=map(swr,10,40,1,70-1);
+      }
+int curseur_entier=curseur_map/5;        //un caractère 5 de large
+int curseur_modulo=curseur_map%5;        //modulo
+int start_curseur=6;                     //position de départ en colonne sur l'afficheur
+int end_curseur=6+curseur_entier;        //position de fin en caractère plein
+while(start_curseur!=end_curseur)        //remplissage
+{
+  lcd.setCursor(start_curseur, ligne_curseur);
+  lcd.write(byte(5));
+  start_curseur++;
+}
+  lcd.setCursor(start_curseur, ligne_curseur);  //positionne le caractère modulo 5
+  lcd.write(byte(curseur_modulo));
+  start_curseur++;
+  while(start_curseur!=20)                      //complète jusqu'a 19 en caractère vide
+  {
+  lcd.setCursor(start_curseur, ligne_curseur); 
+  lcd.write(byte(0));
+  start_curseur++;
+  }
+/*  lcd.setCursor(8,3);    //affiche le résulat courant
+  lcd.print(swr / 10);
+  lcd.print(F("."));
+  lcd.print(swr % 10);
+  lcd.print(F("  "));
+  */
+}
+
+
+ #endif
 
 //--------------------------------------- gestion de la commande de déclenchement du mode recherche
 
-void checkSwr()
+void checkSwr(byte force)
 {
   int s=mesureSWR();
-  if (P>Atu.Pset) {
+   if ((P>Atu.Pset) || (force==1)) {
     digitalWrite(rede_pin, HIGH);
-    if ((s>Atu.swrSet*10) && (Research==0)) {
+    if (((s>Atu.swrSet*10) && (Research==0)) || (force==1)) {
       search_atu();
       Research=1;
     }
@@ -193,6 +380,7 @@ void checkSwr()
   else Research=0;
   delay(measureInterval);
 }
+
 
 //--------------------------------------- gestion de l'EEPROM
 
@@ -233,30 +421,22 @@ void ReadEeprom()
 
 void setAtu(byte L, byte C, byte hz, byte balun)
 {
-  send_gpio(LRAdr, 0x09, L); 
-  send_gpio(CRAdr, 0x09, swapp(C)); 
+  sendGpio(~L,~C);
+  
+ // sendGpioMcp(mcpLRAdr, 0x09, ~L); 
+ // sendGpioMcp(mcpCRAdr, 0x09, ~C); 
+  //digitalWrite(hzlz_pin, hz^1);
+  //digitalWrite(balun_pin, balun^1);
+
   digitalWrite(hzlz_pin, hz);
   digitalWrite(balun_pin, balun);
+
 }
 
 
 
-void LCUpDown(byte lu, int pas)
-//Exemple : LCUpDown (1,-1) décemente C
-//Exemple : LCUpDown (0,10) incrémente L de 10
-{
-  if (lu==0) {
-    lval+=pas;
-  }
-  else 
-  {
-    cval+=pas;
-  }
-  send_gpio(LRAdr, 0x09, lval); 
-  send_gpio(CRAdr, 0x09, swapp(cval));
-}
 
-
+/*
 byte swapp(byte cval)         //valable que pour platine L+C
 {
   byte c=0;
@@ -270,12 +450,34 @@ byte swapp(byte cval)         //valable que pour platine L+C
   bitWrite(c, 7, bitRead(cval, 0));
   return c;
 }
+*/
 
-void send_gpio(byte adresse, byte registre, byte valeur)
+
+void sendGpio(byte Lvalue,byte Cvalue)
+{
+  if (McpPcf==0){
+      sendGpioMcp(mcpLRAdr, 0x09, Lvalue); 
+      sendGpioMcp(mcpCRAdr, 0x09, Cvalue);
+  }
+  else
+  {
+    sendGpioPCF(pcfLRAdr,Lvalue);
+    sendGpioPCF(pcfCRAdr,Cvalue);
+  }
+}
+
+void sendGpioMcp(byte adresse, byte registre, byte valeur)
 {
   Wire.beginTransmission(adresse);      // start talking to the device
   Wire.write(registre);                   // select the IODIR register
   Wire.write(valeur);                   // set register value-all high, sets all pins as outputs on MCP23008
+  Wire.endTransmission();            // stop talking to the devicevice
+}
+
+void sendGpioPCF(byte adresse, byte valeur)
+{
+  Wire.beginTransmission(adresse);      // start talking to the device
+  Wire.write(valeur);                   // select the IODIR register
   Wire.endTransmission();            // stop talking to the devicevice
 }
 
@@ -293,7 +495,7 @@ void search_atu()
   int hz=0;
   int Lvalue;
   int Cvalue;
-
+ 
   
   Serial.write('R');  //démarrage d'une recherche large
   Serial.write('0'); 
@@ -309,12 +511,14 @@ void search_atu()
       Cvalue=0;
       do
       {
-        digitalWrite(hzlz_pin, hzlz);
-        send_gpio(LRAdr, 0x09, Lvalue); 
-        send_gpio(CRAdr, 0x09, swapp(Cvalue));
+        digitalWrite(hzlz_pin, hz^1);
+        sendGpio(~Lvalue,~Cvalue);
+        //sendGpioMcp(mcpLRAdr, 0x09, ~Lvalue); 
+        //sendGpioMcp(mcpCRAdr, 0x09, ~Cvalue);
         delay(searchDelay);
-        current_swr=mesureSWR();
         SendSwr(current_swr);
+        current_swr=mesureSWR();
+        //SendSwr(current_swr);
         if (current_swr<swr_min) {
           cval=Cvalue;
           lval=Lvalue;
@@ -372,18 +576,21 @@ void search_atu()
   Serial.write(0x0a);
   waitTxComplete();
 
+digitalWrite(hzlz_pin, hzlz^1);
+
   Lvalue= Lstart;
   do
   {
     Cvalue=Cstart;
     do
     {
-      digitalWrite(hzlz_pin, hzlz);
-      send_gpio(LRAdr, 0x09, Lvalue); 
-      send_gpio(CRAdr, 0x09, swapp(Cvalue));
+      sendGpio(~Lvalue,~Cvalue);
+      //sendGpioMcp(mcpLRAdr, 0x09, ~Lvalue); 
+      //sendGpioMcp(mcpCRAdr, 0x09, ~Lvalue);
       delay(searchDelay);
+       SendSwr(current_swr);
       current_swr=mesureSWR();
-      SendSwr(current_swr);
+      //SendSwr(current_swr);
       if (current_swr<swr_min) {
         cval=Cvalue;
         lval=Lvalue;
@@ -396,9 +603,10 @@ void search_atu()
   }
   while ( (Lvalue<=Lstop)&&(swr_min>10)); 
 
-  digitalWrite(hzlz_pin, hzlz);
-  send_gpio(LRAdr, 0x09, lval); 
-  send_gpio(CRAdr, 0x09, swapp(cval));
+  //digitalWrite(hzlz_pin, hzlz^1);
+  sendGpio(~lval,~cval);
+  //sendGpioMcp(mcpLRAdr, 0x09, ~lval); 
+  //sendGpioMcp(mcpCRAdr, 0x09, ~cval);
 
   Atu.L=lval;
   Atu.C=cval;
@@ -511,6 +719,11 @@ S45 : compte rendu de la recherche swr x10 => 4.5
   else Serial.print(swr);
   Serial.write(0x0a);
   waitTxComplete();
+#ifdef AFFLCD
+  AtuLcd.swr=swr;         //testale
+  PrintSwr();
+ #endif
+  
 }
 
 void SendPwr()
@@ -525,9 +738,22 @@ P000/999 : x10 -> 99.9W
     Serial.print((int)(P*10));
     Serial.write(0x0a);
     waitTxComplete();
+    #ifdef AFFLCD
+     AtuLcd.p=P*10; 
+     PrintPwr();  
+    #endif
+    
   }
 }
 
+
+
+ 
+    
+   
+
+
+    
 void commandInput()
 {
   if (Serial.available() > 0) {
@@ -605,6 +831,7 @@ void commandExecute()
   {
   case 'b' : 
     Atu.balun=cmdValue;
+    //digitalWrite(balun_pin, Atu.balun^1);
     digitalWrite(balun_pin, Atu.balun);
     break;
   case 'w' : 
@@ -613,27 +840,30 @@ void commandExecute()
   case 'u' : 
     if (cmdValue==0) {
       Atu.L++;
-      LCUpDown (0, 1);
+      //LCUpDown (0, 1);      
     }
     else
     {
       Atu.C++;
-      LCUpDown (1, 1);
+      //LCUpDown (1, 1);
     }
+     sendGpio(~Atu.L,~Atu.C);
     break;
   case 'd' : 
     if (cmdValue==0) {
       Atu.L--;
-      LCUpDown (0, -1);
+     // LCUpDown (0, -1);
     }
     else
     {
       Atu.C--;
-      LCUpDown (1, -1);
+     // LCUpDown (1, -1);
     }
+    sendGpio(~Atu.L,~Atu.C);
     break;
   case 'h' : 
     Atu.Hz=cmdValue;
+    //digitalWrite(hzlz_pin, Atu.Hz^1);
     digitalWrite(hzlz_pin, Atu.Hz);
     break; 
   case 'c' : 
@@ -643,7 +873,8 @@ void commandExecute()
     Atu.OffsetdBmBridge=cmdValue;
     break; 
   case 'n' : 
-    search_atu();
+    //search_atu();  //hs car pas de cde rs485
+    checkSwr(1);    // test force swr
     break; 
   case 'v' : 
     Atu.saveAuto=cmdValue;
@@ -722,5 +953,31 @@ void waitTxComplete()
  //delay(20);
 }
 
+void scanI2C()
+{
+Serial.println ();
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+ 
+  Wire.begin();
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+      {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+      } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+}
 
 
